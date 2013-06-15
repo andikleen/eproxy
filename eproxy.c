d6e4f5530a09d357551d21da8a67a8612d2cc833
Andi Kleen <ak@linux.intel.com>
2013-06-14 17:13:32 -0700

    add intel copyright

--- eproxy.c
+++ eproxy.c
@@ -1,4 +1,16 @@
 /* 
+ * Copyright (c) 2013, Intel Corporation
+ * Author: Andi Kleen
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
  * Simple port forwarder
  * proxy inport outip outport
  * Uses pipes to splice two sockets together. This should give something
